b9199b489fdac55fc49b11a4c254e3364ef35148
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-04-30 13:50:47 +0200

    interaction with button

---
+++ Assets/Scripts/Hotspot.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Hotspot : MonoBehaviour
+{
+    GameObject CurrentSphere;
+    GameObject TargetSphere;
+
+    public void OnHotspotClick()
+    {
+        Debug.Log("Hotspot clicked!");
+    }
+}
